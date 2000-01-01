921662d85b3dc3a424dac2567619f9ebe5fcaa2e
root <root@beaglebone.(none)>
2000-01-01 01:24:19 +0000

    slight bug in pwm file

--- QuadServer/server/pwm.py
+++ QuadServer/server/pwm.py
@@ -1,10 +1,13 @@
+import os
 pins = ((8,13,18),(8,19,19),(9,14,14),(9,16,15),(9,21,16),(9,22,17))
-path = '/sys/devices/ocp.2/P{0}_{1}.{2}/duty'
+pwmpath = '/sys/devices/ocp.2/P{0}_{1}.{2}/duty'
 
 class signal:
   pwmfile=None
   def __init__(self,number):
-    self.pwmfile=open(path.format(*pins[number]))
-  def setwidth(self,width):
+    if not os.path.exists(pwmpath.format(*pins[number])):
+      os.system('echo MULTC > /sys/devices/bone_capemgr.8/slots')
+    self.pwmfile=open(pwmpath.format(*pins[number]))
+  def set_width(self,width):
     self.pwmfile.write(str(width))
 
