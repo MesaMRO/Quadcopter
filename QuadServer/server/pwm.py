ecdbf542d44c3227f696a7c8cd1b3724ea791d2c
mr1337357 <mr1337357@gmail.com>
2013-08-10 12:24:18 -0700

    somehow this file got lost in transition

---
+++ QuadServer/server/pwm.py
@@ -0,0 +1,10 @@
+pins = ((8,13,18),(8,19,19),(9,14,14),(9,16,15),(9,21,16),(9,22,17))
+path = '/sys/devices/ocp.2/P{0}_{1}.{2}/duty'
+
+class signal:
+  pwmfile=None
+  def __init__(self,number):
+    pwmfile=open(path.format(*pins[number]))
+  def setwidth(self,width):
+    pwmfile.write(str(width))
+
